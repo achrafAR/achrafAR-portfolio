e617bb830e5d6cf40d669d51901405556ab24258
achrafAR <achraf.alrachini@gmail.com>
2024-03-08 16:40:49 +0400

    start the services page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import HomePage from './pages/HomePage.js';
-import AboutMe from './components/AboutMe/AboutMe.js';
+import AboutMe from './pages/AboutMe.js';
+import Services from './pages/Services.js';
+
+
 function App() {
   return (
     <div className="App">
       <HomePage/>
       <AboutMe/>
+      <Services/>
     </div>
   );
 }
---
+++ src/components/Services/Service.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./service.css";
+
+function Service() {
+  return (
+    <div className="services-container">
+      <div className="services">
+        <div className="awesome-services">
+          <h1>My Awesome Service</h1>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration.
+          </p>
+        </div>
+        <div className="services-card-container">
+          <div className="services-card-details"></div>
+          <div className="services-card-details"></div>
+          <div className="services-card-details"></div>
+          <div className="services-card-details"></div>
+          <div className="services-card-details"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
