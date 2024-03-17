67b59ba417a3d242bef536194b67ba2225590a7c
achrafAR <achraf.alrachini@gmail.com>
2024-03-18 02:56:15 +0400

    continue the contact page

--- src/components/Contact/ContactMe.js
+++ src/components/Contact/ContactMe.js
@@ -8,20 +8,22 @@ function ContactMe() {
       <div className="contact-form-image">
         <div className="contact-text-form">
           <div className="conatct-text">
-            <h1>Hire ME</h1>
+            <h1>Hire ME.</h1>
             <p>
-              I am available for freelance work. Connect with me via phone:
-              01923 088574 or email: admin@example.com
-            </p>
+    I am available for freelance work. Connect with me via phone:
+    <span>01923 088574 </span>
+    or email:
+    <span>admin@example.com</span>
+</p>
           </div>
           <div className="conatct-form">
             <input type="text" id="name" name="name" placeholder="Your Name" required />
-            <input type="text" id="email" name="email" placeholder=" Your Email" required />
+            <input type="text" id="email" name="email" placeholder="Your Email" required />
             <input type="text" id="subject" name="subject" placeholder="Write a Subject" required />
             <input type="text" id="message" name="message" placeholder="Your Message" required />
           </div>
           <div className="contact-button">
-            <button>Submit</button>
+            <button>SUBMIT</button>
           </div>
         </div>
         <div className="contact-image">
